a4dc7029a8eac9d9a0640dac3f27f999811e3518
Matheus Silva <matheusmmjm989@gmail.com>
2025-08-23 14:19:20 -0300

    mudando pastas

---
+++ paginas/exe/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBerrzBXG7xEPY-zokNSzxsOLcCXHvSKyA",
+  authDomain: "caridade-6464e.firebaseapp.com",
+  projectId: "caridade-6464e",
+  storageBucket: "caridade-6464e.firebasestorage.app",
+  messagingSenderId: "916897058334",
+  appId: "1:916897058334:web:906d1343b3523b3a9ca80f",
+};
+firebase.initializeApp(firebaseConfig);
