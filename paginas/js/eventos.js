5e90f33c6bebaaa5f4efdd99653ce51c7d22c264
Matheus Silva <matheusmmjm989@gmail.com>
2025-11-18 06:08:55 -0300

    criacao da pagina eventos

---
+++ paginas/js/eventos.js
@@ -0,0 +1,21 @@
+function extractYouTubeID(url) {
+    const regex = /(?:v=|\.be\/)([^&\n?#]+)/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".event-card").forEach(card => {
+        const link = card.dataset.youtube;
+        const id = extractYouTubeID(link);
+
+        if (id) {
+            const thumbnail = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+            card.style.backgroundImage = `url('${thumbnail}')`;
+
+            card.addEventListener("click", () => {
+                window.open(link, "_blank");
+            });
+        }
+    });
+});
